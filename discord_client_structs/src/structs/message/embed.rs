18123479834bfa742cb1aef0b137d2982435053c
UwUDev <mmimi0593@gmail.com>
2025-02-23 11:59:11 +0100

    [STRUCT] Embed types

--- discord_client_structs/src/structs/message/embed.rs
+++ discord_client_structs/src/structs/message/embed.rs
@@ -8,7 +8,10 @@ use serde::{Deserialize, Serialize};
 #[builder(setter(into, strip_option), default)]
 pub struct Embed {
     pub title: Option<String>,
-    pub r#type: String,
+    #[serde(default)]
+    #[serde(deserialize_with = "deserialize_embed_type")]
+    #[serde(serialize_with = "serialize_embed_type")]
+    pub r#type: EmbedType,
     pub description: Option<String>,
     pub url: Option<String>,
     #[serde(default)]
@@ -29,6 +32,71 @@ pub struct Embed {
     pub reference_id: Option<u64>,
     pub flags: Option<u64>,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum EmbedType {
+    Rich,
+    Image,
+    Video,
+    Gifv,
+    Article,
+    Link,
+    PollResult,
+    Unknown(String),
+}
+
+impl EmbedType {
+    pub fn as_str(&self) -> &str {
+        match self {
+            EmbedType::Rich => "rich",
+            EmbedType::Image => "image",
+            EmbedType::Video => "video",
+            EmbedType::Gifv => "gifv",
+            EmbedType::Article => "article",
+            EmbedType::Link => "link",
+            EmbedType::PollResult => "poll_result",
+            EmbedType::Unknown(s) => s,
+        }
+    }
+}
+
+impl From<&str> for EmbedType {
+    fn from(s: &str) -> Self {
+        match s {
+            "rich" => EmbedType::Rich,
+            "image" => EmbedType::Image,
+            "video" => EmbedType::Video,
+            "gifv" => EmbedType::Gifv,
+            "article" => EmbedType::Article,
+            "link" => EmbedType::Link,
+            "poll_result" => EmbedType::PollResult,
+            _ => EmbedType::Unknown(s.to_string()),
+        }
+    }
+}
+
+impl Default for EmbedType {
+    fn default() -> Self {
+        EmbedType::Rich
+    }
+}
+
+fn deserialize_embed_type<'de, D>(deserializer: D) -> Result<EmbedType, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(EmbedType::from(s.as_str()))
+}
+
+fn serialize_embed_type<S>(embed_type: &EmbedType, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(embed_type.as_str())
+}
+
+
 
 #[derive(Debug, Deserialize, Serialize, Clone, Builder, Default)]
 #[builder(setter(into, strip_option), default)]
