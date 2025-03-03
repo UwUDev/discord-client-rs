2ec6eb655d478aaad939cb1cb693c4c9fe035068
UwUDev <mmimi0593@gmail.com>
2025-03-03 13:17:03 +0100

    [REST] Invite context + fixed context encoding

--- discord_client_rest/src/structs/context.rs
+++ discord_client_rest/src/structs/context.rs
@@ -1,3 +1,5 @@
+use base64::Engine;
+use discord_client_structs::structs::channel::invite::Invite;
 use serde_json::json;
 
 pub trait ContextHeader {
@@ -9,6 +11,13 @@ pub struct NoContext;
 
 #[derive(Debug, Clone)]
 pub struct NewGroupDmContext;
+
+#[derive(Debug, Clone)]
+pub struct InviteContext {
+    pub location_guild_id: u64,
+    pub location_channel_id: u64,
+    pub location_channel_type: u8,
+}
 
 impl ContextHeader for NoContext {
     fn get_header_value(&self) -> String {
@@ -22,17 +31,51 @@ impl ContextHeader for NewGroupDmContext {
     }
 }
 
+impl ContextHeader for InviteContext {
+    fn get_header_value(&self) -> String {
+        json!({
+            "location": "Join Guild",
+            "location_guild_id": self.location_guild_id.to_string(),
+            "location_channel_id": self.location_channel_id.to_string(),
+            "location_channel_type": self.location_channel_type,
+        })
+        .to_string()
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Context {
     NoContext,
     NewGroupDmContext,
+    InviteContext(InviteContext),
 }
 
 impl ContextHeader for Context {
     fn get_header_value(&self) -> String {
-        match self {
+        let ctx = match self {
             Context::NoContext => NoContext.get_header_value(),
             Context::NewGroupDmContext => NewGroupDmContext.get_header_value(),
+            Context::InviteContext(ctx) => ctx.get_header_value(),
+        };
+
+        base64::engine::general_purpose::STANDARD.encode(ctx.as_bytes())
+    }
+}
+
+impl From<Invite> for Context {
+    fn from(invite: Invite) -> Self {
+        match invite.guild {
+            Some(guild) => {
+                let channel = invite.channel.unwrap();
+                let id = channel.id;
+                let r#type = channel.r#type;
+                Context::InviteContext(InviteContext {
+                    location_guild_id: guild.id,
+                    location_channel_id: id,
+                    location_channel_type: r#type,
+                })
+            },
+            None => Context::NoContext,
         }
     }
 }
