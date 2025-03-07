0943260c0ad3797b2118f2ec00228c385fb93205
UwUDev <mmimi0593@gmail.com>
2025-03-07 17:21:08 +0100

    [REST] Fixed token parser

--- discord_client_structs/src/parser.rs
+++ discord_client_structs/src/parser.rs
@@ -1,11 +1,27 @@
-use base64::{Engine as _, engine::general_purpose::STANDARD as BASE64};
+use base64::Engine as _;
+use base64::engine::{GeneralPurpose, GeneralPurposeConfig};
 use chrono::{DateTime, TimeZone, Utc};
 use std::error::Error;
 
 pub fn parse_id_from_token(token: &str) -> Result<u64, Box<dyn Error>> {
+    let alphabet = base64::alphabet::STANDARD;
+    let custom_engine = GeneralPurpose::new(
+        &alphabet,
+        GeneralPurposeConfig::new().with_decode_allow_trailing_bits(true),
+    );
+
     let parts: Vec<&str> = token.split('.').collect();
-    let id_str = BASE64.decode(parts[0])?;
-    let id_str = String::from_utf8(id_str)?;
+
+    if parts.is_empty() {
+        return Err("Invalid token format".into());
+    }
+
+    let first_part = parts[0];
+    let padding_needed = (4 - (first_part.len() % 4)) % 4;
+    let padded_part = format!("{}{}", first_part, "=".repeat(padding_needed));
+
+    let decoded = custom_engine.decode(padded_part.as_bytes())?;
+    let id_str = String::from_utf8(decoded)?;
     Ok(id_str.parse()?)
 }
 
