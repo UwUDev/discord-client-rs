2b82853089d06940a7a03be330f14e3af87cf62d
UwUDev <mmimi0593@gmail.com>
2025-05-28 18:51:40 +0200

    [MACRO] Flags macro

--- discord_client_macros/src/lib.rs
+++ discord_client_macros/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{Data, DeriveInput, Fields, GenericArgument, PathArguments, Type, parse_macro_input};
+use convert_case::{Case, Casing};
+
 
 fn is_u64_type(ty: &Type) -> bool {
     if let Type::Path(type_path) = ty {
@@ -326,4 +328,192 @@ pub fn derive_enum_from_string(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(Flags, attributes(flag_enum))]
+pub fn derive_flags(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    generate_flags_impl(input)
+}
+
+fn generate_flags_impl(input: DeriveInput) -> TokenStream {
+    let struct_name = &input.ident;
+    let mut output = quote! {};
+
+    if let Data::Struct(data) = &input.data {
+        if let Fields::Named(fields) = &data.fields {
+            for field in &fields.named {
+                let field_name = field.ident.as_ref().unwrap();
+                let field_type = &field.ty;
+
+                let flag_enum_attr = field.attrs.iter()
+                    .find(|attr| attr.path().is_ident("flag_enum"));
+
+                if let Some(attr) = flag_enum_attr {
+                    let is_option = is_option_u64_type(field_type);
+                    let is_u64 = is_u64_type(field_type);
+
+                    if !is_option && !is_u64 {
+                        return syn::Error::new_spanned(
+                            field_type,
+                            "Field with #[flag_enum] must be of type u64 or Option<u64>"
+                        ).to_compile_error().into();
+                    }
+
+                    let flags_content = attr.parse_args::<syn::LitStr>()
+                        .expect("flag_enum attribute must contain a string with flag definitions")
+                        .value();
+
+                    let enum_name = format_ident!("{}", 
+                        field_name.to_string().to_case(Case::Pascal));
+
+                    let flag_enum = generate_simple_flag_enum(&enum_name, &flags_content);
+                    let conversion_methods = generate_flag_conversion_methods(
+                        struct_name, field_name, &enum_name, is_option
+                    );
+
+                    output = quote! {
+                        #output
+                        #flag_enum
+                        #conversion_methods
+                    };
+                }
+            }
+        }
+    }
+
+    output.into()
+}
+
+fn generate_simple_flag_enum(enum_name: &syn::Ident, flags_content: &str) -> proc_macro2::TokenStream {
+    let flag_data: Vec<_> = flags_content.split(',')
+        .map(|s| {
+            let parts: Vec<_> = s.trim().split('=').collect();
+            let name = format_ident!("{}", parts[0].trim());
+            let value: u8 = parts[1].trim().parse().expect("Invalid flag value");
+            (name, value)
+        })
+        .collect();
+
+    let flag_variants = flag_data.iter().map(|(name, value)| {
+        quote! { #name = #value }
+    });
+
+    let extract_flag_checks = flag_data.iter().map(|(name, value)| {
+        quote! {
+            if value & (1u64 << #value) != 0 {
+                flags.push(#enum_name::#name);
+            }
+        }
+    });
+
+    let from_bit_arms = flag_data.iter().map(|(name, value)| {
+        quote! { #value => Some(#enum_name::#name) }
+    });
+
+    quote! {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+        #[repr(u8)]
+        pub enum #enum_name {
+            #(#flag_variants,)*
+        }
+        
+        impl #enum_name {
+            pub fn bit_value(&self) -> u64 {
+                1u64 << (*self as u8)
+            }
+            
+            pub fn from_bit_position(bit: u8) -> Option<Self> {
+                match bit {
+                    #(#from_bit_arms,)*
+                    _ => None,
+                }
+            }
+            
+            pub fn extract_flags(value: u64) -> Vec<Self> {
+                let mut flags = Vec::new();
+                #(#extract_flag_checks)*
+                flags
+            }
+            
+            pub fn combine_flags(flags: &[Self]) -> u64 {
+                flags.iter().fold(0u64, |acc, flag| acc | flag.bit_value())
+            }
+        }
+    }
+}
+
+fn generate_flag_conversion_methods(
+    struct_name: &syn::Ident,
+    field_name: &syn::Ident,
+    enum_name: &syn::Ident,
+    is_option: bool,
+) -> proc_macro2::TokenStream {
+    let getter_name = format_ident!("get_{}", field_name);
+    let setter_name = format_ident!("set_{}", field_name);
+    let has_flag_name = format_ident!("has_{}", field_name);
+    let add_flag_name = format_ident!("add_{}", field_name);
+    let remove_flag_name = format_ident!("remove_{}", field_name);
+
+    if is_option {
+        quote! {
+            impl #struct_name {
+                pub fn #getter_name(&self) -> Vec<#enum_name> {
+                    self.#field_name
+                        .map(|value| #enum_name::extract_flags(value))
+                        .unwrap_or_default()
+                }
+                
+                pub fn #setter_name(&mut self, flags: Vec<#enum_name>) {
+                    if flags.is_empty() {
+                        self.#field_name = None;
+                    } else {
+                        self.#field_name = Some(#enum_name::combine_flags(&flags));
+                    }
+                }
+                
+                pub fn #has_flag_name(&self, flag: #enum_name) -> bool {
+                    self.#field_name
+                        .map(|value| value & flag.bit_value() != 0)
+                        .unwrap_or(false)
+                }
+                
+                pub fn #add_flag_name(&mut self, flag: #enum_name) {
+                    let current = self.#field_name.unwrap_or(0);
+                    self.#field_name = Some(current | flag.bit_value());
+                }
+                
+                pub fn #remove_flag_name(&mut self, flag: #enum_name) {
+                    if let Some(current) = self.#field_name {
+                        let new_value = current & !flag.bit_value();
+                        self.#field_name = if new_value == 0 { None } else { Some(new_value) };
+                    }
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl #struct_name {
+                pub fn #getter_name(&self) -> Vec<#enum_name> {
+                    #enum_name::extract_flags(self.#field_name)
+                }
+                
+                pub fn #setter_name(&mut self, flags: Vec<#enum_name>) {
+                    self.#field_name = #enum_name::combine_flags(&flags);
+                }
+                
+                pub fn #has_flag_name(&self, flag: #enum_name) -> bool {
+                    self.#field_name & flag.bit_value() != 0
+                }
+                
+                pub fn #add_flag_name(&mut self, flag: #enum_name) {
+                    self.#field_name |= flag.bit_value();
+                }
+                
+                pub fn #remove_flag_name(&mut self, flag: #enum_name) {
+                    self.#field_name &= !flag.bit_value();
+                }
+            }
+        }
+    }
 }
